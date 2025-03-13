552d8b5fd0a4a4cb49d324df4bfdfe310cff8a77
Fernanda Cardoso Serafim <fernanda.cs20@aluno.ifsc.edu.br>
2025-03-13 12:20:36 -0300

    Atividade Banco de dados 13/03/25
    Nossa primeira conexão com o banco de dados

---
+++ BancoDeDados/src/main/java/principal/BancoDeDados.java
@@ -0,0 +1,13 @@
+
+
+package principal;
+
+import java.sql.SQLException;
+
+public class BancoDeDados {
+
+    public static void main(String[] args) throws SQLException {
+        ConexaoBD conexaoBD = new ConexaoBD();
+        conexaoBD.conectarBanco();
+    }
+}
